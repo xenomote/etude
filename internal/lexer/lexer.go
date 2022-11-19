f6bcd8c45493987c71c8b73cf2b944520e715f47
Alexander Williamson <aw277@st-andrews.ac.uk>
2022-11-19 18:37:33 +0000

    add initial parser details

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -42,12 +42,146 @@ func New(r io.Reader) Lexer {
 		pos: Position{1, 1},
 		buf: &strings.Builder{},
 	}
-	l.Read()
+	l.read()
 
 	return l
 }
 
-func (l *Lexer) Read() {
+func (l *Lexer) Want(tokens ...Token) (Token, error) {
+	got, err := l.Next()
+
+	for _, want := range tokens {
+		if want == got {
+			return want, nil
+		}
+	}
+
+	if err != nil {
+		return got, err
+	}
+
+	return got, UnexpectedToken
+}
+
+func (l *Lexer) Next() (Token, error) {
+	for !l.end && unicode.IsSpace(l.last) {
+		l.read()
+	}
+	l.Clear()
+
+	if l.end {
+		return l.fail(UnexpectedEOF)
+	}
+
+	switch l.last {
+	case '{', '}', '[', ']', '(', ')', '?', '@', '#', '.', ',', '+', '-', '*', '/', '^', '%':
+		return l.emit(Token(l.last))
+
+	case '=':
+		return l.either(EQUALS, DOUBLE_EQUALS)
+
+	case ':':
+		return l.either(COLON, COLON_EQUALS)
+
+	case '!':
+		return l.either(EXCLAIM, EXCLAIM_EQUALS)
+
+	case '<':
+		return l.either(LESS, LESS_EQUALS)
+
+	case '>':
+		return l.either(GREATER, GREATER_EQUALS)
+
+	case '"':
+		return l.string()
+	}
+
+	if unicode.IsDigit(l.last) {
+		return l.number()
+	}
+
+	if unicode.IsLetter(l.last) {
+		return l.identifier()
+	}
+
+	return l.fail(NotFound)
+}
+
+func (l *Lexer) Text() string {
+	s := l.buf.String()
+	if !l.end {
+		s = s[:len(s)-1]
+	}
+
+	return s
+}
+
+func (l *Lexer) Clear() {
+	l.buf.Reset()
+	if !l.end {
+		l.buf.WriteRune(l.last)
+	}
+}
+
+func (l *Lexer) emit(t Token) (Token, error) {
+	l.got = t
+	l.read()
+	return t, nil
+}
+
+func (l *Lexer) fail(err error) (Token, error) {
+	l.got = ERROR
+	return ERROR, err
+}
+
+func (l *Lexer) either(a, b Token) (Token, error) {
+	l.read()
+
+	if l.last == '=' {
+		return l.emit(b)
+	}
+
+	l.got = a
+	return a, nil
+}
+
+func (l *Lexer) string() (Token, error) {
+	for {
+		l.readEscape()
+
+		if l.end {
+			return l.fail(UnexpectedStringEOF)
+		}
+
+		if l.last == '\n' {
+			return l.fail(UnexpectedStringNewline)
+		}
+
+		if l.last == '"' {
+			return l.emit(STRING)
+		}
+	}
+}
+
+func (l *Lexer) number() (Token, error) {
+	l.read()
+	return NUMBER, nil
+}
+
+func (l *Lexer) identifier() (Token, error) {
+	l.read()
+	return IDENTIFIER, nil
+}
+
+func (l *Lexer) readEscape() {
+	l.read()
+	if l.last == '\\' {
+		l.read()
+		l.read()
+	}
+}
+
+func (l *Lexer) read() {
 	c, _, err := l.src.ReadRune()
 	if err == io.EOF {
 		l.end = true
@@ -71,138 +205,4 @@ func (l *Lexer) Read() {
 	}
 
 	l.last = c
-}
-
-func (l *Lexer) Next() (Token, error) {
-	for !l.end && unicode.IsSpace(l.last) {
-		l.Read()
-	}
-	l.Clear()
-
-	if l.end {
-		return l.Fail(UnexpectedEOF)
-	}
-
-	switch l.last {
-	case '{', '}', '[', ']', '(', ')', '?', '@', '#', '.', ',', '+', '-', '*', '/', '^', '%':
-		return l.Emit(Token(l.last))
-
-	case '=':
-		return l.Either(EQUALS, DOUBLE_EQUALS)
-
-	case ':':
-		return l.Either(COLON, COLON_EQUALS)
-
-	case '!':
-		return l.Either(EXCLAIM, EXCLAIM_EQUALS)
-
-	case '<':
-		return l.Either(LESS, LESS_EQUALS)
-
-	case '>':
-		return l.Either(GREATER, GREATER_EQUALS)
-
-	case '"':
-		return l.String()
-	}
-
-	if unicode.IsDigit(l.last) {
-		return l.Number()
-	}
-
-	if unicode.IsLetter(l.last) {
-		return l.Identifier()
-	}
-
-	return l.Fail(NotFound)
-}
-
-func (l *Lexer) Emit(t Token) (Token, error) {
-	l.got = t
-	l.Read()
-	return t, nil
-}
-
-func (l *Lexer) Fail(err error) (Token, error) {
-	l.got = ERROR
-	return ERROR, err
-}
-
-func (l *Lexer) Either(a, b Token) (Token, error) {
-	l.Read()
-
-	if l.last == '=' {
-		return l.Emit(b)
-	}
-
-	l.got = a
-	return a, nil
-}
-
-func (l *Lexer) String() (Token, error) {
-	for {
-		l.ReadEscape()
-
-		if l.end {
-			return l.Fail(UnexpectedStringEOF)
-		}
-
-		if l.last == '\n' {
-			return l.Fail(UnexpectedStringNewline)
-		}
-
-		if l.last == '"' {
-			return l.Emit(STRING)
-		}
-	}
-}
-
-func (l *Lexer) ReadEscape() {
-	l.Read()
-	if l.last == '\\' {
-		l.Read()
-		l.Read()
-	}
-}
-
-func (l *Lexer) Number() (Token, error) {
-	l.Read()
-	return NUMBER, nil
-}
-
-func (l *Lexer) Identifier() (Token, error) {
-	l.Read()
-	return IDENTIFIER, nil
-}
-
-func (l *Lexer) Want(tokens ...Token) (Token, error) {
-	got, err := l.Next()
-
-	for _, want := range tokens {
-		if want == got {
-			return want, nil
-		}
-	}
-
-	if err != nil {
-		return got, err
-	}
-
-	return got, UnexpectedToken
-}
-
-func (l *Lexer) Text() string {
-	s := l.buf.String()
-	if !l.end {
-		s = s[:len(s)-1]
-	}
-
-	return s
-}
-
-func (l *Lexer) Clear() {
-	l.buf.Reset()
-	if !l.end {
-		l.buf.WriteRune(l.last)
-	}
 }
